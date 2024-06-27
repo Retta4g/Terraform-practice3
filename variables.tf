8e5eae13826fd1513ecc095258dc1fd5b203dc5e
Messeret Woldehanna <messeret@Messerets-Laptop.local>
2024-06-27 11:24:47 -0500

    update with new files

--- variables.tf
+++ variables.tf
@@ -1,17 +1,16 @@
-
-# INPUts
+# INPUTS
 variable "instance_type" {
   default = "t2.micro"
 }
-variable "instane_nmae" {
-  default = "helloworld"
+variable "instance_name" {
+  default = "HelloWorld"
 }
 variable "instance_ami" {
-    default = "ami-01b799c439fd5516a"
+  default = "ami-01b799c439fd5516a"
 }
 variable "subnet_name" {
   default = "tf-example"
 }
-variable "VPC_name" {
-    default = "tf-example"
+variable "vpc_name" {
+  default = "tf-example"
 }
